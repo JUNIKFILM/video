35408b35dab0e64d72cf5c26e62c88b98ed3e99f
JUNIKFILM <junikflim@gmail.com>
2025-03-09 08:34:28 -0700

    Update plantillavideofav.js

--- plantillavideofav.js
+++ plantillavideofav.js
@@ -83,9 +83,10 @@ function addStyles() {
       flex-shrink: 0;
     }
     
-    .notification-content h3 {
+    .notification-content h2 {
       margin: 0 0 8px 0;
       font-size: 18px;
+       color: rgb(255, 255, 255);
     }
     
     .notification-content p {
@@ -169,3 +170,6 @@ function initConnectionNotifications() {
 
 // Run the initialization when the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', initConnectionNotifications);
+
+
+
